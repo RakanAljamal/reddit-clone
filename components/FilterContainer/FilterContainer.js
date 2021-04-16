c07c70d7aa7fb08d6d94e50629de79fe99182ce7
raljamal <rakanaljamal@gmail.com>
2021-04-17 02:33:49 +0300

    Finishing Filter Container

--- components/FilterContainer/FilterContainer.js
+++ components/FilterContainer/FilterContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from './style.module.scss';
+import FilterButton from "../FilterButton";
 
 const FilterContainer = ({title}) => {
     return <div className={styles.filterBackgroundContainer}>
         <h2 className={styles.filterTitle}>{title}</h2>
         <div className={styles.filterContainer}>
-
+            <FilterButton title='Hot' icon='Hot'/>
+            <FilterButton title='Everywhere' noHover showArrow/>
+            <FilterButton title='Top' icon='Top' hideBackground/>
+            <FilterButton title='New' icon='New' hideBackground/>
+            <FilterButton icon='Triple-Dots' hideBackground lightHover/>
+            <FilterButton icon='Layout-Switch' hideBackground showArrow lightHover/>
         </div>
     </div>
 }
